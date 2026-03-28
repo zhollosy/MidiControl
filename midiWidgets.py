79b213bbc35a2f2abd681598c2dda1af7d81a22b
HZola <zhollosy@gmail.com>
2026-03-28 10:17:33 +0100

    Migrates main application entry to PyQt6
    Updates `midiWidgets.py` to use PyQt6 imports and API calls. Refactors the main layout to be an instance variable for improved access and consistency, and updates the application execution method.

--- midiWidgets.py
+++ midiWidgets.py
@@ -1,11 +1,11 @@
 import sys
-# from PyQt5.QtWidgets import *
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QSlider
-from PyQt5.QtWidgets import QDial, QLCDNumber, QLabel, QProgressBar, QPushButton
-from PyQt5.QtWidgets import QHBoxLayout
-# from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtCore import Qt
+# from PyQt6.QtWidgets import *
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QSlider
+from PyQt6.QtWidgets import QDial, QLCDNumber, QLabel, QProgressBar, QPushButton
+from PyQt6.QtWidgets import QHBoxLayout
+# from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import QtWidgets, QtCore, QtGui
+from PyQt6.QtCore import Qt
 
 import QMCWidgets
 
@@ -21,10 +21,10 @@ class MainWindow(QMainWindow):
         self.width = 680
         self.height = 500
 
-        layoutH_main = QHBoxLayout()
+        self.layoutH_main = QHBoxLayout()
 
         self.ADSR_Widget = QMCWidgets.QMCAmpADSR()
-        layoutH_main.addWidget(self.ADSR_Widget)
+        self.layoutH_main.addWidget(self.ADSR_Widget)
 
         widgets = [
             QDial,
@@ -35,10 +35,10 @@ class MainWindow(QMainWindow):
             QSlider]
 
         for w in widgets:
-            layoutH_main.addWidget(w())
+            self.layoutH_main.addWidget(w())
 
         widget = QWidget()
-        widget.setLayout(layoutH_main)
+        widget.setLayout(self.layoutH_main)
 
         self.setCentralWidget(widget)
 
@@ -64,7 +64,7 @@ def main():
     w = MainWindow()
     w.show()
 
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 
 if __name__ == '__main__':
