c013334e3e8fb46d462c89c0361ed49da2fd7526
HZola <zhollosy@gmail.com>
2026-03-28 17:51:27 +0100

    Integrates new widgets into QMCWidgets package

--- QMCWidgets/__init__.py
+++ QMCWidgets/__init__.py
@@ -1,3 +1,8 @@
 """Qt Midi Controller Widgets"""
 
 from .QMCAmpADSR import QMCAmpADSR
+from .QMCSlider import QMCSlider
+from .QMCButton import QMCButton
+from .QMCDial import QMCDial
+from .QMCGroup import QMCGroup
+from .QMCKeyboard import QMCKeyboard
